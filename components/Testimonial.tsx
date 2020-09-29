8f3473bdb404ec0261106152f0e7f3bd011fe83a
Benjamin Kinga <somethingscripted@gmail.com>
2020-09-28 20:36:46 -0500

    changed max-w-2xl to w-1/2

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -2,7 +2,7 @@ import { MdEmail } from "react-icons/md";
 
 const Testimonial = ({ name, title, email, statement, imgSrc }) => {
 	return (
-		<div className="flex bg-white shadow-lg rounded-lg max-w-full md:max-w-2xl mx-0 my-4 md:mx-4 md:my-0">
+		<div className="flex bg-white shadow-lg rounded-lg w-full md:w-1/2 mx-0 my-4 md:mx-4 md:my-0">
 			<div className="flex items-start px-4 py-6 w-full">
 				<img className="w-20 h-20 rounded-full object-cover mr-4 shadow border-2" src={imgSrc} alt={name} />
 				<div className="w-full">
