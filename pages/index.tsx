c4da5108493e4b4f0d9e1935d4cf3c0188521fff
emanuallan <allanserna@gatech.edu>
2020-09-30 11:18:57 -0500

    fixed index links

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,20 +12,35 @@ const IndexPage = () => {
 						<Menu />
 
 						<div className="gap-6 grid sm:grid-cols-2 lg:grid-cols-3 grid-cols-1">
-							<img src="/gifs/social.gif" className="rounded  mx-auto" alt="Social Image Generator" />
-							<img src="/gifs/companysearch.gif" className="rounded   mx-auto" alt="Company Search" />
+							<a href="/projects/tallysight">
+								<img src="/gifs/social.gif" className="rounded  mx-auto" alt="Social Image Generator" />
+							</a>
+							<a href="/projects/scholars">
+								<img src="/gifs/companysearch.gif" className="rounded   mx-auto" alt="Company Search" />
+							</a>
+							<a href="/projects">
+								<img src="/gifs/newbalance.gif" className="rounded   mx-auto" alt="New Balance" />
+							</a>
 
-							<img src="/gifs/newbalance.gif" className="rounded   mx-auto" alt="New Balance" />
+							<a href="/projects">
+								<img src="/gifs/db-dispersive.gif" className="rounded   mx-auto" alt="Dispersive DB" />
+							</a>
 
-							<img src="/gifs/db-dispersive.gif" className="rounded   mx-auto" alt="New Balance" />
-
-							<img src="/gifs/studentresources.gif" className="rounded   mx-auto" alt="New Balance" />
-
-							<img
-								src="/gifs/dispersivetree.gif"
-								className="rounded  mx-auto"
-								alt="Dispersive Interactive Tree"
-							/>
+							<a href="/projects/scholars">
+								<img
+									src="/gifs/studentresources.gif"
+									className="rounded   mx-auto"
+									alt="Scholars
+								"
+								/>
+							</a>
+							<a href="/projects">
+								<img
+									src="/gifs/dispersivetree.gif"
+									className="rounded  mx-auto"
+									alt="Dispersive Interactive Tree"
+								/>
+							</a>
 						</div>
 					</div>
 
